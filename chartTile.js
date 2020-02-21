a319db4a77a66aa7936f9ea6e66c2a826dbac469
Nathan Lee <nathan.l.lee123@gmail.com>
2020-02-21 01:01:46 -0800

    Marginally working singular tile

--- chartTile.js
+++ chartTile.js
@@ -3,25 +3,32 @@ Input: x y points, y axis min/max, sensorlist
 
 */
 
-export class ChartTile {
-
-    initialize(xAxis, yAxis, canvasHeight, canvasWidth) {
+class ChartTile {
+    debugger;
+    constructor(xAxis, yAxis, canvasHeight, canvasWidth) {
         //Y Axis Variables
-        this.yAxis = [-20, 20];
+        this.yAxis = yAxis;
         this.yAxisHeight = this.yAxis[1] - this.yAxis[0];
         this.YScale = canvasHeight / this.yAxisHeight;
 
         //X Axis Variables
-        this.xAxis = [25, 50]
+        this.xAxis = xAxis
         this.xAxisHeight = this.xAxis[1] - this.xAxis[0];
         this.XScale = canvasWidth / this.xAxisHeight;
         this.coordData = []
 
         //Create a blank canvas
         this.chartTile = document.createElement("canvas")
-        chartTile.width = canvasWidth;
-        chartTile.height = canvasHeight;
-        document.body.appendChild(this.chartTile)
+        document.getElementById("chart").appendChild(this.chartTile);
+
+        //Set width and height
+        this.canvasWidth = canvasWidth;
+        this.canvasHeight = canvasHeight;
+
+        this.chartTile.style.width = this.canvasWidth;
+        this.chartTile.style.height = this.canvasHeight;
+        console.log(canvasHeight)
+
     }
 
     loadData(dataRange, sensors) {
@@ -30,16 +37,21 @@ export class ChartTile {
     }
 
     plotData() {
-        this.ctx = chartTile.getContext('2d');
+        this.ctx = this.chartTile.getContext('2d');
         this.ctx.beginPath();
         this.ctx.lineJoin = "round";
         this.ctx.strokeStyle = "black";
 
-        for(var b = 0; b < this.coordData.length; b++) {
-            this.newX = (this.data[i][0] - this.xAxis[0]) * this.XScale
-            this.newY = (this.data[i][1] - this.yAxis[0]) * this.YScale
+        for(var b = 0; b < this.data.length; b++) {
+            this.newX = (this.data[b][0] - this.xAxis[0]) * this.XScale
+            this.newY = (this.data[b][1] - this.yAxis[0]) * this.YScale
+            //console.log(1 / this.YScale)
+            console.log()
+            console.log(this.newX, this.newY)
             this.ctx.lineTo(this.newX, this.newY)
         }
         this.ctx.stroke();
     }
 }
+
+export {ChartTile}
--- coordinator.js
+++ coordinator.js
@@ -1,8 +1,13 @@
 /*
 This bad boi handles the loading and unloading of tiles
+Handles global variables -
+Timeline range
+Handles current events
 */
 
-import {ChartTile} from "chartTile.js"
+import {ChartTile} from "./chartTile.js"
 
-var testTile = new ChartTile()
-testTile.initialize([25, 50], [-20, 20], 100, 80)
+var testTile = new ChartTile([25, 50], [-20, 20], 80, 100)
+testTile.loadData([20, 55], ['test'])
+testTile.plotData()
+//testTile.initialize()
